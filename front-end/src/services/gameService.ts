7d53d4356f73b72f162db876c644f6a87730a4fb
DuongTruong <truongdinhdongduong@gmail.com>
2025-04-12 00:10:42 +0700

    fix: fix game-service, connection signalR and gameHub

--- front-end/src/services/gameService.ts
+++ front-end/src/services/gameService.ts
@@ -26,7 +26,8 @@ class GameService {
         }
 
         this.connection = new HubConnectionBuilder()
-            .withUrl(apiUrl)
+            // .withUrl(apiUrl)
+            .withUrl('http://localhost:5071/gameHub')
             .withAutomaticReconnect([0, 1000, 2000, 5000])
             .configureLogging(LogLevel.Warning)
             .build();

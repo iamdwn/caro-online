d4f19153cf965738d7a402cd02ba16381921e54a
DuongTruong <truongdinhdongduong@gmail.com>
2025-04-14 12:29:21 +0700

    revert

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,18 +1,12 @@
+
 import React from 'react';
 import { Game } from './components/Game';
-import { BrowserRouter } from 'react-router-dom';
 
 function App() {
     return (
-        <BrowserRouter>
-            <div className="App">
-                <Game 
-                    connection={null}
-                    roomId=""
-                    isHost={false}
-                />
-            </div>
-        </BrowserRouter>
+        <div className="App">
+            <Game />
+        </div>
     );
 }
 

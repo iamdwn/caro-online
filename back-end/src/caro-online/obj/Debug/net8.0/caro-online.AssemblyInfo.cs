0d7d6dadda086c6b5fd67fa8524b86ce54e5495f
DuongTruong <truongdinhdongduong@gmail.com>
2025-05-15 23:21:15 +0700

    config

--- back-end/src/caro-online/obj/Debug/net8.0/caro-online.AssemblyInfo.cs
+++ back-end/src/caro-online/obj/Debug/net8.0/caro-online.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("caro-online")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12b4415618ae1c84bb0d99235d52b27e9fb2f07d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd34b3f1ff3fe39a39ac6d780817c1dadcfbec04")]
 [assembly: System.Reflection.AssemblyProductAttribute("caro-online")]
 [assembly: System.Reflection.AssemblyTitleAttribute("caro-online")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
